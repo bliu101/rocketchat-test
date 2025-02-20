51eb6de1a4d37a054206e1e288cae6ec514bb113
Bridgette Liu <bridgetteliu@Bridgettes-MacBook-Pro.local>
2025-02-20 11:33:22 -0500

    more specific

--- app.py
+++ app.py
@@ -124,12 +124,13 @@ def generate_quiz():
     #                    'give the key. In the key explain each answer like a helpful tutor, assuming no previous knowledge.')
     query = f'If the message delimited in triple astriks is not a question, '\
             f'ask the chatter to ask a question regarding the Grade 6 Math Scope and Sequence Outline. '\
-            f'Answer the given question delimited in triple astriks. Answer based on the file,' \
+            f'Answer the given question delimited in triple astriks. First, give a straightforward answer based on the file,' \
             f'and then synthesize to give possible solutions and idea brainstorming.'\
-            f'***{message}***'
+            f'This is the message: ***{message}***'
 
     system_constant = ('Prompt the user in a friendly manner to ask a question '
-                       'regarding the Grade 6 Math Scope and Sequence Outline. '
+                       'regarding the Grade 6 Math Scope and Sequence Outline and specify '
+                       'each unit and te topics in each unit '
                        'Do not outwardly specify whether the given message is a question.'
                        'Act as a helpful teaching assistant that will give detailed '
                        'information on the question based on the uploaded file.')
