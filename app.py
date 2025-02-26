8e9daa6db75cd8d6f1933f39e9f2a5010237a2fd
Bridgette Liu <bridgetteliu@Bridgettes-MBP.lan>
2025-02-26 00:20:58 -0500

    agents added

--- app.py
+++ app.py
@@ -9,11 +9,64 @@ from llmproxy import generate, pdf_upload
 
 app = Flask(__name__)
 
+SESSION_ID = 'bridgette-agent'
+
 @app.route('/')
 def hello_world():
    return jsonify({"text":'Hello from Koyeb - you reached the main page!'})
 
 @app.route('/query', methods=['POST'])
+def agent_critque(query):
+    system = """
+    You are an AI agent designed critque a resume written by a college student or new graduate.
+    The goal is to create a resume that will standout and hit relevant industry words and
+    sythesize on the experiences listed on the given resume.
+
+    You have two options:
+    ### Option 1 ###
+    If you see any lacking parts, respond with pointing out any lacking information or unneccessary information,
+    suggestions for simplifying, improving, and optimizing the resume..
+    
+    ### Option 2 ###
+    2. If you don't see any issues with the resume, respond with "No changes needed".
+    """
+
+    response = generate(model = '4o-mini',
+        system = system,
+        query = query,
+        temperature=0.3,
+        lastk=10,
+        session_id=SESSION_ID,
+        rag_usage = False)
+
+    try:
+        return response['response']
+    except Exception as e:
+        print(f"Error occured with parsing output: {response}")
+        raise e
+    return 
+
+def agent_builder(query):
+
+    system = """
+    You are an AI agent designed to make a resume.
+    """
+
+    response = generate(model = '4o-mini',
+        system = system,
+        query = query,
+        temperature=0.3,
+        lastk=10,
+        session_id=SESSION_ID,
+        rag_usage = False)
+
+    try:
+        return response['response']
+    except Exception as e:
+        print(f"Error occured with parsing output: {response}")
+        raise e
+
+    return
 def main():
     data = request.get_json() 
 
@@ -30,43 +83,62 @@ def main():
     print(f"Message from {user} : {message}")
 
     response_pdf = pdf_upload(
-        path = 'prematriculation_credits.pdf',
-        session_id = 'bridgette-rag-test',
+        path = 'resume.pdf',
+        session_id = SESSION_ID,
         strategy = 'smart'
     )
 
-    query = f'Given this message delimited in triple astriks, reply by giving the number of credits for each listed class, '\
-            f'based on the uploaded file. ***{message}***'\
+    query = f'Generate a new resume based on the information, work experience, leadership experience, schooling'\
+            f'and tailor it based on the following user input delimited in triple astriks. ***{message}***'\
+            f'If the user input is not related to a career, job industry, or description inform the user in a friendly manner '\
+            f'that you can edit the given resume to fit the given job description. '
 
-    system_constant = ('If the user message is not related to the AP/IB prematricualtion credits at Tufts, '
-                        'prompt the user in a friendly manner to list their AP/IB test and their school '
-                       'Arts and Sciences or Engineering so that you can analyze what pre-matriculation credits '
-                       'they have earned based on the uploaded file '
-                       'Answer related questions or lists of courses properly based on the uploaded file by '
-                       'breaking down each test score and the number of credits recieved for it '
-                       'Ensure that the college is specified: Arts and Sciences or Engineering before giving an answer'
-                       'Act as a welcoming and helpful guide for incoming freshmen who may be confused.')
+    # system_constant = ('If the user message is not related to the AP/IB prematricualtion credits at Tufts, '
+    #                    'prompt the user in a friendly manner to list their AP/IB test and their school '
+    #                    'Arts and Sciences or Engineering so that you can analyze what pre-matriculation credits '
+    #                    'they have earned based on the uploaded file '
+    #                    'Answer related questions or lists of courses properly based on the uploaded file by '
+    #                    'breaking down each test score and the number of credits recieved for it '
+    #                    'Ensure that the college is specified: Arts and Sciences or Engineering before giving an answer'
+    #                    'Act as a welcoming and helpful guide for incoming freshmen who may be confused.')
+
+    agents = [agent_builder, agent_crique]
+
+    max_iterations = 1
+
+    i=0
+    while i < max_iterations:
+
+        # flip between agent coder and QA
+        active_agent = agents[i%2]
 
 
+        query = active_agent(query)
 
-    print(f'QUERY::: {query}\n')
+        if query == "$$EXIT$$":
+            break
+
+        i+=1
+
+
+    # print(f'QUERY::: {query}\n')
     
-    response = generate(
-        model='4o-mini',
-        system=system_constant,
-        query=query,
-        temperature=0.0,
-        lastk=0,
-        session_id='bridgette-rag-test',
-        rag_usage=True,
-        rag_threshold='0.2',
-        rag_k=4
-    )
+    # response = generate(
+    #     model='4o-mini',
+    #     system=system_constant,
+    #     query=query,
+    #     temperature=0.0,
+    #     lastk=0,
+    #     session_id='bridgette-rag-test',
+    #     rag_usage=True,
+    #     rag_threshold='0.2',
+    #     rag_k=4
+    # )
     
-    response_text = response['response']
-    print(response_text)
+    # response_text = response['response']
+    # print(response_text)
 
-    return jsonify({"text": response_text})
+    # return jsonify({"text": response_text})
 
 # @app.route('/upload_pdf', methods=['POST'])
 # def upload_pdf():
